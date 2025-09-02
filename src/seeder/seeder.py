207c4934805f8318dd4385c1f9c1c5f5b25492fc
Valerio Morelli <valeriomorelli50@gmail.com>
2025-09-02 18:44:50 +0200

    Implement seedings

--- src/seeder/seeder.py
+++ src/seeder/seeder.py
@@ -1,5 +1,6 @@
+from random import random
 from src.connector.MongoDB import MongoDBConnector
-from typing import Any
+from typing import Any, Callable
 from faker import Faker
 
 
@@ -7,26 +8,49 @@ class DocSeeder:
     def __init__(self, schema: dict[str, "EntrySeeder"]) -> None:
         self.schema = schema
 
-    def seed(self) -> dict:
+    def __seed(self) -> dict:
         return {
             k: v
             for k, v in {k: v.seed() for k, v in self.schema.items()}.items()
             if v != None
         }
 
+    def seed(self, amount: int | None = None) -> dict | list[dict]:
+        return (
+            self.__seed() if amount is None else [self.__seed() for _ in range(amount)]
+        )
+
 
 class EntrySeeder:
-    def __init__(self, type: "str | list | DocSeeder", optional: bool = False) -> None:
+    def __init__(
+        self,
+        type: "str | list|range | DocSeeder | Callable[[Faker],Any]",
+        p: float = 1,
+        unique: bool = False,
+    ) -> None:
         self.type = type
-        self.optional = optional
+        self.p = p
         self.__fake = Faker("it_IT")
+        self.unique = unique
+        if unique:
+            assert isinstance(type, (list, range))
+            self.counter = 0
 
     def seed(self) -> Any:
-        if self.optional and self.__fake.boolean():
+        if random() > self.p:
             return None
         if isinstance(self.type, DocSeeder):
             return self.type.seed()
-        elif isinstance(self.type, list):
+        elif isinstance(self.type, Callable):
+            return self.type(self.__fake)
+        elif isinstance(self.type, (list, range)):
+            self.type = list(self.type)
+            if self.unique:
+                if self.counter >= len(self.type):
+                    raise Exception("No more values")
+                val = self.type[self.counter]
+                self.counter += 1
+                return val
             return self.__fake.random_element(self.type)
         else:
             return self.__fake.__getattr__(self.type)()
